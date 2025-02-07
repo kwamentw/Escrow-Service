62bff63d7b3fa000a7a60ceed2ea0f3c68e07468
kwamentw <antwi1496@hotmail.com>
2025-02-06 20:35:55 -0400

    Escrow-service

--- src/Escrow.sol
+++ src/Escrow.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.25;
 
 contract Escrow {
+    event ArbitratorAdded(address newArb);
     enum AssetType{ERC20, ERC721, Native}
     enum EscrowStatus{NONE, SETTLED, UNSETTLED}
 
@@ -14,12 +15,22 @@ contract Escrow {
         uint256 deadline;
         bool buyerConfirm;
         bool sellerConfirm;
+        EscrowStatus status;
     }
 
     mapping (address user => uint256 points) reputation;
-    mapping (uint256 id => bool status) escrowExist;
-    address[] public arbitrators;
+    mapping (uint256 id => EscrowInfo escrow) escrows;
+    mapping (address user => bool status) arbitrators;
     uint256 insurancePool;
 
-    function createEscrow() external {}
+    function createEscrow(EscrowInfo memory newEscrow) external {
+        require(newEscrow.buyer == address(0));
+        require(newEscrow.status == EscrowStatus.NONE);
+    }
+
+    function addArbitrator(address newArbitrator) external {
+        require(arbitrators[newArbitrator]== false, "already an arbitrator");
+        arbitrators[newArbitrator] = true;
+        emit ArbitratorAdded(newArbitrator);
+    }
 }
